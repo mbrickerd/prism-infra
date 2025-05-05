feddf378fe421420ab6d32130f843d63f9f134ee
Mallory <mallorybrickerd@gmail.com>
2025-05-05 14:36:09 +0200

    feat: Key Vault (#19)

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,5 +1,22 @@
 locals {
   create_shared_variables = var.environment == "dev"
+
+  secrets = {
+    "storage-connection-string" = {
+      name         = "storage-connection-string"
+      content      = module.storage_account.primary_connection_string
+      content_type = "connection-string"
+    },
+    "storage-account-key" = {
+      content      = module.storage_account.primary_access_key
+      content_type = "account-key"
+    },
+    "eventhub-connection-string" = {
+      content      = module.eventhub_namespace.default_primary_connection_string
+      content_type = "connection-string"
+    }
+  }
+
   tags = merge(var.tags, {
     managed_by_terraform = true
     environment          = var.environment
